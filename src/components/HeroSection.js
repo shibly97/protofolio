f31c05fcaacf460bd0c06edb4f9b3cb5baac079c
Shibly <shibly2016@gmail.com>
2020-12-11 14:00:22 +0530

    logo with div

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,6 @@ import './HeroSection.css'
 import {Button} from './Button'
 import video from '../video/video-1.mp4'
 import profile from '../image/pic-3.jpg'
-import {Link} from 'react-router-dom'
 
 function HeroSection() {
     return (
@@ -12,8 +11,7 @@ function HeroSection() {
             <video src={video} autoPlay loop muted></video>
             <img id='profile-img' src={profile} alt='profile'/>
             <div className = 'hero-btns'>
-            <Link to='/about'>
-                <Button to='/about' className='btns' buttonStyle='btn--hero' buttonSize='btn--ex-large'>About Me</Button></Link>
+                <Button to='/about' className='btns' buttonStyle='btn--hero' buttonSize='btn--ex-large'>About Me</Button>
             </div>
         </div>
     )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -34,61 +34,61 @@ const Skills = () => {
             <h2>Programming Languages</h2>
                 
                 <div className="python">
-                <img alt="logo" src={pythoni} className="python-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={pythoni} className="python-img"></img></div>
                 <h5>Python</h5></div>
 
                 <div className="js">
-                <img alt="logo" src={js} className="js-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={js} className="js-img"></img></div>
                 <h5>JavaScript</h5></div>
 
                 <div className="c#">
-                <img alt="logo" src={c} className="c#-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={c} className="c#-img"></img></div>
                 <h5>C#</h5></div>
             </div>
             <div className="front-end lang">
                 <h2>Front-End</h2>
 
-                <div className={html}>
-                <img alt="logo" src={html} className="html-img"></img>
+                <div className='html'>
+                <div className="lang-img-con"><img alt="logo" src={html} className="html-img"></img></div>
                 <h5>HTML</h5></div>
 
                 <div className="css">
-                <img alt="logo" src={css} className="css-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={css} className="css-img"></img></div>
                 <h5>CSS 3</h5></div>
                 
                 <div className="react">
-                <img alt="logo" src={react} className="react-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={react} className="react-img"></img></div>
                 <h5>React</h5></div>
                 
                 <div className="redux">
-                <img alt="logo" src={redux} className="redux-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={redux} className="redux-img"></img></div>
                 <h5>Redux</h5></div>
                 
                 <div className="jquery">
-                <img alt="logo" src={jquery} className="jquery-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={jquery} className="jquery-img"></img></div>
                 <h5>jquery</h5></div>
                 
                 <div className="bootstrap">
-                <img alt="logo" src={bootstrap} className="bootstrap-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={bootstrap} className="bootstrap-img"></img></div>
                 <h5>Bootstrap</h5></div>
 
                 <div className="sass">
-                <img alt="logo" src={sass} className="sass-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={sass} className="sass-img"></img></div>
                 <h5>Sass</h5></div>
 
                 <div className="d3">
-                <img alt="logo" src={d3} className="d3-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={d3} className="d3-img"></img></div>
                 <h5>D3</h5></div>
             </div>
             <div className="back-end lang">
                 <h2>Back-End</h2>
 
                 <div className="node">
-                <img alt="logo" src={node} className="node-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={node} className="node-img"></img></div>
                 <h5>Node js</h5></div>
 
                 <div className="express">
-                <img alt="logo" src={express} className="express-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={express} className="express-img"></img></div>
                 <h5>Express</h5></div>
 
                 {/* <div className="php">
@@ -96,7 +96,7 @@ const Skills = () => {
                 <h5>PHP</h5></div> */}
 
                 <div className="django">
-                <img alt="logo" src={django} className="django-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={django} className="django-img"></img></div>
                 <h5>Django</h5></div>
 
             </div>
@@ -104,15 +104,15 @@ const Skills = () => {
                 <h2>DataBase</h2>
 
                 <div className="mongo">
-                <img alt="logo" src={mongodb} className="mongo-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={mongodb} className="mongo-img"></img></div>
                 <h5>MongoDB</h5></div>
 
                 <div className="mysql">
-                <img alt="logo" src={mysql} className="mysql-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={mysql} className="mysql-img"></img></div>
                 <h5>Mysql</h5></div>
 
                 <div className="mssql">
-                <img alt="logo" src={mssql} className="mssql-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={mssql} className="mssql-img"></img></div>
                 <h5>Mssql</h5></div>
 
             </div>
@@ -120,19 +120,19 @@ const Skills = () => {
                 <h2>Other</h2>
 
                 <div className="git">
-                <img alt="logo" src={git} className="git-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={git} className="git-img"></img></div>
                 <h5>GIT</h5></div>
 
                 <div className="github">
-                <img alt="logo" src= {github} className="github-img"></img>
+                <div className="lang-img-con"><img alt="logo" src= {github} className="github-img"></img></div>
                 <h5>GitHub</h5></div>
 
                 <div className="heroku">
-                <img alt="logo" src={heroku} className="heroku-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={heroku} className="heroku-img"></img></div>
                 <h5>Heroku</h5></div>
 
                 <div className="figma">
-                <img alt="logo" src={figma} className="figma-img"></img>
+                <div className="lang-img-con"><img alt="logo" src={figma} className="figma-img"></img></div>
                 <h5>Figma</h5></div>
 
             </div>
